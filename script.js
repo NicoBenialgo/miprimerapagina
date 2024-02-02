d69597a09568b7f44979eb0304db5e8ec419221e
Nicolas Federico Benialgo <nicolasbenialgo@hotmail.com>
2024-02-01 21:09:50 -0300

    Cambios en los estilos y scripts
    Se seleccionaron los scripts que deben estar activados, quedando al margen todos los que fueron trabajados en la cursada. Estructuración del CSS.

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-let texto1 = "Curso Egg";
+// PARTE 1 - Código usado para realizar cambios en el contenido de etiquetas
+// let texto1 = "Curso Egg";
+// let texto = document.querySelector("#cambio-nombre");
+// texto.innerHTML = texto1;
+// let textoH2 = texto.innerText;
+// console.log(textoH2);
+// if(textoH2 == texto1) {
+//     texto.innerHTML = "Juegos";
+// };
+// FIN DE PARTE 1
 
-let texto = document.querySelector("#cambio-nombre");
-texto.innerHTML = texto1;
-
-// vigesimocuarto intento del condicionar
-let textoH2 = texto.innerText;
-console.log(textoH2);
-
-if(textoH2 == texto1) {
-    texto.innerHTML = "Juegos";
-};
-
-// segundo intento de la función de cambiar texto
-let nombre = "Nicolas Benialgo";
-let pais = "Argentina";
-let edad = 18;
-let gustos = "viajar, el helado y aprender programación";
-
-let parrafo = document.querySelector(".parrafo1");
+// PARTE 2 - Código usado para cambiar contenido de una etiqueta p usando una función
+// let nombre = "Nicolas Benialgo";
+// let pais = "Argentina";
+// let edad = 18;
+// let gustos = "viajar, el helado y aprender programación";
+// let parrafo = document.querySelector(".parrafo1");
 
 // function cambiarTexto(nombre, pais, edad, gustos) {
 //     let contenido = `Mi nombre es ${nombre}, nací y crecí dentro del país de ${pais} y tengo ${edad} años. Mis gustos son: ${gustos}.`;
@@ -25,10 +22,11 @@ let parrafo = document.querySelector(".parrafo1");
 // };
 
 // parrafo.innerText = cambiarTexto(nombre, pais, edad, gustos);
+// FIN DE PARTE 2
 
-// Check btn js
-// let menu_responsive = document.querySelector(".checkbtn");
-// menu_responsive.onclick = function () {
-//     navBar = document.querySelector(".navbar");
-//     navBar.classList.toggle("active");
-// };
+// PARTE 3 - Interactividad para la barra de navegación en pantallas menores a 768px
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+    navBar = document.querySelector(".navbar");
+    navBar.classList.toggle("active");
+};
